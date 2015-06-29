295d8cbc8a86ff8dc3695193f762d9322da958b3
Andy Reagan <andy@andyreagan.com>
2015-06-29 15:00:28 -0400

    search pages updated, comments removed

--- js/activityphrases.js
+++ js/activityphrases.js
@@ -11640,31 +11640,32 @@ function loadCsv() {
         stateFeatures = topojson.feature(geoJson,geoJson.objects.states).features;
         if (!--csvLoadsRemaining) initializePlotPlot();
     });
-    d3.text("data/ALL_FOODS_FOR_Rcleaned4R_NEWEST_1CUPLIQ.csv", function (text) {
-        var tmp = text.split("\n")
-        foodCals = tmp.map(function(d) { return parseFloat(d.split(",")[3]); }).slice(1,1604);
-        foodNames = tmp.map(function(d) { return d.split(",")[0].slice(1,d.split(",")[0].length-1); }).slice(1,1604);;
+
+    d3.text("data/foodList_lemmatized_no_quotes.csv", function (text) {
+        var tmp = text.split("\n").slice(1,451);
+        foodCals = tmp.map(function(d) { return parseFloat(d.split(",")[3]); });
+	foodNames = tmp.map(function(d) { return d.split(",")[0]; });
         if (!--csvLoadsRemaining) initializePlotPlot();
     });
-    d3.text("data/PHYS_ACT_2011-12edits_MTAs.csv", function (text) {
-        var tmp = text.split("\n").slice(1,1000000);
-        actCals = tmp.map(function(d) { return d.split(",")[3]; });
+    d3.text("data/activityList_lemmatized_no_quotes.csv", function (text) {
+        var tmp = text.split("\n").slice(1,299);
+        actCals = tmp.map(function(d) { return parseFloat(d.split(",")[2]); });
         actNames = tmp.map(function(d) { return d.split(",")[0]; });
         if (!--csvLoadsRemaining) initializePlotPlot();
     });
-    d3.text("data/finalStateActivityMatrix.csv", function (text) {
-        var tmp = text.split("\n").slice(1,13420);
+    d3.text("data/stateActivitiesMatrix_lemmatized.csv", function (text) {
+        var tmp = text.split("\n").slice(1,299);
         stateAct = tmp.map(function(d) { return d.split(",").slice(1,1000); });
         if (!--csvLoadsRemaining) initializePlotPlot();
     });
-    d3.text("data/stateFoodsMatrix_1CUPLIQ.csv", function (text) {
-        var tmp = text.split("\n").slice(1,1604);
+    d3.text("data/stateFoodsMatrix_lemmatized.csv", function (text) {
+        var tmp = text.split("\n").slice(1,451);
         stateFood = tmp.map(function(d) { return d.split(",").slice(1,1000); });
         if (!--csvLoadsRemaining) initializePlotPlot();
     });
-    d3.text("data/just_flux.csv", function (text) {
+    d3.text("data/caloric_balance06292015.csv", function (text) {
         var tmp = text.split("\n").slice(1,1000000);
-        stateFlux = tmp.map(function(d) { return parseFloat(d.split(",")[1]); });
+        stateFlux = tmp.map(function(d) { return parseFloat(d.split(",")[1]); }).slice(0,49);
         if (!--csvLoadsRemaining) initializePlotPlot();
     });
 };
